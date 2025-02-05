011160902df6c0b4724c89205e4a6344c0dbf7d0
mombewa <ombewamaurice79@gmail.com>
2025-02-05 13:41:54 +0300

    implement limit for clients to less or equal to ten

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net"
+	"os"
 	"sync"
 )
 
@@ -22,10 +25,9 @@ type Server struct {
 	mutex      sync.Mutex
 }
 
-
-func main(){
+func main() {
 	file, err := os.ReadFile("linux.txt")
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 	}
 
@@ -41,5 +43,28 @@ func main(){
 	ln, _ := net.Listen("tcp", ":8989")
 	defer ln.Close()
 
+	server := NewServer()
 
+	go server.run()
+
+	// fmt.Printf("Listening on the port :%s\n", port)
+	// fmt.Println("Listening on the port :8989")
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("Error accepting connection: %v", err)
+			continue
+		}
+
+		s := server
+		clientCount := len(s.clients)
+		if clientCount >= 10 {
+			conn.Write([]byte("Chat is full (max 10 clients). Please try again later.\n"))
+			conn.Close()
+			continue
+		}
+
+		go server.handleClient(conn)
+	}
 }
